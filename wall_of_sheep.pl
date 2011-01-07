6a6f7f427038036875ce5eac4d0daad353b10ed7
William Orr <will@worrbase.com>
2011-01-07 14:04:03 -0500

    Reveals more of the password every offense
    - Configurable!

--- wall_of_sheep.pl
+++ wall_of_sheep.pl
@@ -5,6 +5,12 @@ use 5.010;
 
 use Term::ANSIColor;
 
+use constant REVEAL_DIVISOR => 30;
+use constant PASS_LENGTH => 20;
+use constant PASS_SUFFIX => '*' x PASS_LENGTH;
+
+my %idiots;
+
 #Attempt to prevent screen blanking
 printf ("\033[9;%ld]", 0);
 
@@ -16,7 +22,7 @@ system(($^O eq 'MSWin32') ? 'cls' : 'clear');
 
 while(my $line = <>){
 	#Hope you like regexp.
-	if ($line =~ /^(\w+) : (\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}):(\d{1,10}) -> USER: (.+) PASS: (.+)/){
+	if ($line =~ /^(\w+) : (\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}):(\d{1,10}) -> USER: (.+) PASS: (.*)/){
 		my $protocol = $1;
 		my $ip = "$2.$3.$4.$5";
 		my $port = $6;
@@ -24,13 +30,14 @@ while(my $line = <>){
 		my $pass = $8;
 		my $ip_addr = pack("C4", $2,$3,$4,$5);
 		my ($hostname) = (gethostbyaddr($ip_addr, 2))[0];
+		$idiots{$ip}++;
 		
 		my $scrubbed_pass = "";
 		if(length($pass) > 1){
-			$scrubbed_pass=substr($pass,0,1)."*****";
+			$scrubbed_pass=substr($pass,0,($idiots{$ip} / REVEAL_DIVISOR) + 1).PASS_SUFFIX;
 		}
 		else{
-			$scrubbed_pass="*******";
+			$scrubbed_pass=PASS_SUFFIX;
 		}
 		
 		pick_color($protocol);	
@@ -43,8 +50,8 @@ while(my $line = <>){
 			$hostname = "";
 		}
 		$user = sprintf '%.*s', 16, $user;
-		$pass = sprintf '%.*s', 7, $scrubbed_pass;
-		print pack("A10 A16 A29 A17 A8", $protocol, $ip, $hostname, $user, $pass);
+		$pass = sprintf '%.*s', PASS_LENGTH, $scrubbed_pass;
+		print pack("A10 A16 A29 A17 A".PASS_LENGTH, $protocol, $ip, $hostname, $user, $pass);
 		print "\n";
 		print color("reset");
 	}
