8a3dc87ca03b1be3dbc7e4b73b4c61aa1f4eb19d
William Orr <will@worrbase.com>
2011-01-07 13:43:07 -0500

    Small perl modifications

--- wall_of_sheep.pl
+++ wall_of_sheep.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use 5.010;
+
 use Term::ANSIColor;
-use Switch;
 
 #Attempt to prevent screen blanking
 printf ("\033[9;%ld]", 0);
@@ -13,12 +14,11 @@ print color("reset");
 #super legit cross-platform screen clear
 system(($^O eq 'MSWin32') ? 'cls' : 'clear');
 
-while(<>){
-	my $line = $_;
-	#Hope you like regex.
-	if ($line =~ m/^(.*) : ([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3}).([0-9]{1,3}):([0-9]{1,10}) -> USER: (.*) PASS: (.*)/){
+while(my $line = <>){
+	#Hope you like regexp.
+	if ($line =~ /^(\w+) : (\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}):(\d{1,10}) -> USER: (.+) PASS: (.+)/){
 		my $protocol = $1;
-		my $ip = $2.".".$3.".".$4.".".$5;
+		my $ip = "$2.$3.$4.$5";
 		my $port = $6;
 		my $user = $7;
 		my $pass = $8;
@@ -52,30 +52,30 @@ while(<>){
 
 sub pick_color{
 	my $protocol = shift;
-	switch ($protocol) {
-		case /TELNET/ { print color("red") }
-		case /RLOGIN/ { print color("red") }
-		case /SSH/ { print color("red") }
-		case /VNC/ { print color("red") }
+	given ($protocol) {
+		when (/TELNET/) { print color("red") }
+		when (/RLOGIN/) { print color("red") }
+		when (/SSH/) { print color("red") }
+		when (/VNC/) { print color("red") }
 		
-		case /SMB/ { print color("magenta") }
-		case /LDAP/ { print color("magenta") }
+		when (/SMB/) { print color("magenta") }
+		when (/LDAP/) { print color("magenta") }
 	
-		case /ICQ/ { print color("green") }
-		case /MSN/ { print color("green") }
-		case /YMSG/ { print color("green") }
-		case /IRC/ { print color("green") }
+		when (/ICQ/) { print color("green") }
+		when (/MSN/) { print color("green") }
+		when (/YMSG/) { print color("green") }
+		when (/IRC/) { print color("green") }
 
-		case /HTTP/ { print color("blue") }
-		case /FTP/ { print color("blue") }
-		case /MYSQL/ { print color("blue") }
+		when (/HTTP/) { print color("blue") }
+		when (/FTP/) { print color("blue") }
+		when (/MYSQL/) { print color("blue") }
 	
-		case /POP/ { print color("yellow") }
-		case /IMAP/ { print color("yellow") }
-		case /SMTP/ { print color("yellow") }
-		case /NNTP/ { print color("yellow") }
+		when (/POP/) { print color("yellow") }
+		when (/IMAP/) { print color("yellow") }
+		when (/SMTP/) { print color("yellow") }
+		when (/NNTP/) { print color("yellow") }
 
-		case /SOCKS5/ {print color("cyan") }
-		case /CVS/ {print color("cyan") }
+		when (/SOCKS5/) {print color("cyan") }
+		when (/CVS/) {print color("cyan") }
 	}
 }
